976bafeed3ae1fa33f8874981e2f40e7649e6162
Soham Mukherjee <sohamhomework@gmail.com>
2026-05-26 14:37:13 -0400

    remove alarmConnector from index.js

--- index.js
+++ index.js
@@ -3,23 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import notifee, {EventType} from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-// Handles alarm snooze/dismiss when app is in background or closed
-notifee.onBackgroundEvent(async ({type, detail}) => {
-  if (type === EventType.ACTION_PRESS && detail.pressAction?.id === 'snooze') {
-    const {snoozeAlarm} = await import('./src/connectors/alarmConnector');
-    await snoozeAlarm(detail.notification?.id ?? '');
-  }
-  if (
-    (type === EventType.ACTION_PRESS && detail.pressAction?.id === 'dismiss') ||
-    type === EventType.DISMISSED
-  ) {
-    const {dismissAlarm} = await import('./src/connectors/alarmConnector');
-    await dismissAlarm(detail.notification?.id ?? '');
-  }
-});
 
 AppRegistry.registerComponent(appName, () => App);
